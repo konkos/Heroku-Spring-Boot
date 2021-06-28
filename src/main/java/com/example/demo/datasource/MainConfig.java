86173763ec3bd37a058269902b6a023a017c2466
kon_k <kon_kostas@windowslive.com>
2021-06-29 00:53:46 +0300

    change DB CONFIG

---
+++ src/main/java/com/example/demo/datasource/MainConfig.java
@@ -0,0 +1,28 @@
+package com.example.demo.datasource;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+public class MainConfig {
+
+    @Bean
+    public HikariDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+        HikariDataSource basicDataSource = new HikariDataSource();
+        basicDataSource.setJdbcUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
+}
