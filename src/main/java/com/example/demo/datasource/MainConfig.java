872db1a42ba167c7e694621f37fa375b9616064e
kon_k <kon_kostas@windowslive.com>
2021-07-06 11:56:31 +0300

    remove ssl-mode

--- src/main/java/com/example/demo/datasource/MainConfig.java
+++ src/main/java/com/example/demo/datasource/MainConfig.java
@@ -16,7 +16,7 @@ public class MainConfig {
 
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
 
         HikariDataSource basicDataSource = new HikariDataSource();
         basicDataSource.setJdbcUrl(dbUrl);
