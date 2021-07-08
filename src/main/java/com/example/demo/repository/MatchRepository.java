5f83e6afb9168fdf3845568e57f7e477d7e92ef1
kon_k <kon_kostas@windowslive.com>
2021-07-08 08:48:48 +0300

    add repository Annotation

--- src/main/java/com/example/demo/repository/MatchRepository.java
+++ src/main/java/com/example/demo/repository/MatchRepository.java
@@ -4,10 +4,12 @@ import com.example.demo.model.Match;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 public interface MatchRepository extends CrudRepository<Match, Long> {
 
     List<Match> getByTeam1OrTeam2OrderByDateDesc(String teamName1, String teamName2, Pageable pageable);
--- src/main/java/com/example/demo/repository/TeamRepository.java
+++ src/main/java/com/example/demo/repository/TeamRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Team;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TeamRepository extends CrudRepository<Team, Long> {
 
     Team findByTeamName(String teamName);
